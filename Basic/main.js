5fc3f742d1da5d6c3432ac5f11f8594bbd97159e
Dawit <dawityab@gmail.com>
2022-01-31 10:51:09 +0100

    last commit of ex 1

---
+++ Basic/main.js
@@ -0,0 +1,9 @@
+
+let temp = Math.floor(Math.random() * 25) + -5;
+
+console.log(`The temperature is ${temp}`);
+ if((temp >= -5) && (temp <= 10) )
+ console.log("The weather is cold");
+ else{
+ console.log("The weather is moderate");
+ }
