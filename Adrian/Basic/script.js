877e13bd35557a44b12550a03fb9ece14e015c7d
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-01-31 12:03:54 +0100

    Basic exercises

---
+++ Adrian/Basic/script.js
@@ -0,0 +1,42 @@
+//EXcercise 1
+
+const number = 30
+const temp = Math.floor((Math.random() * number) - 5);
+
+console.log(`The temperature is ${temp}C.`)
+
+if (temp > -5, temp < 10) {
+    console.log("The wheater is cold")
+} else {
+    console.log("the wheater is moderate")
+}
+
+
+// Excercise 2
+
+let value = [1, 7, -3, 9]
+
+for (let i = 3; i < value.length; i++) {
+    console.log(value[i]);
+}
+
+// Excercise 3
+
+const numberMax = 40
+const tempNew = Math.floor((Math.random() * numberMax) - 5);
+
+console.log(`The temperature is ${tempNew}C.`);
+
+if (tempNew > -6, tempNew < 10) {
+    console.log("The wheater is cold");
+} else if (tempNew > 11, tempNew < 26) {
+    console.log("The wheater is moderate");
+} else {
+    console.log("The wheather is hot");
+}
+
+///
+
+
+
+
