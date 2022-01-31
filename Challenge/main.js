e8d9a88b393757d28242aa39aca51351e8c94138
Dawit <dawityab@gmail.com>
2022-01-31 15:19:54 +0100

    intal commit

---
+++ Challenge/main.js
@@ -0,0 +1,32 @@
+var studentName = prompt('Type your name');
+var studentGrade = prompt('Type your grade');
+
+
+let students = [];
+let mathGrades = [];
+let grade = document.getElementById("grade");
+let container = document.getElementById("container");
+students.push(studentName);
+mathGrades.push(mathGrades);
+document.write(`${studentName}  has reached ${studentGrade} in Math this season.<br\>` );
+
+if(studentGrade < 60){
+
+   grade.innerHTML = "red";
+   container.style.backgroundColor= 'red';
+}
+else if((studentGrade >= 60) && (studentGrade <= 70)){
+    grade.innerHTML = "yellow";
+    container.style.backgroundColor= 'yellow';
+}
+else if(studentGrade > 70){
+    grade.innerHTML = "green";
+    container.style.backgroundColor= 'green';
+    
+}
+else{
+    grade.innerHTML = "blue";
+    container.style.backgroundColor= 'blue';
+}
+console.log(students);
+console.log(mathGrades);
