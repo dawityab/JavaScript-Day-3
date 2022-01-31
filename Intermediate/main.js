14d51e88222f746cb26222736b1673d79ad6a3ae
Dawit <dawityab@gmail.com>
2022-01-31 14:12:55 +0100

    average grading last commit

--- Intermediate/main.js
+++ Intermediate/main.js
@@ -1,4 +1,31 @@
 
+// Student grade
+let grade = prompt();
+
+let studentGrade;
+if(grade < 60){
+    studentGrade = `The class average is ${grade}, the average grade will be F`;
+}
+else if(grade < 70){
+    studentGrade = `The class average is ${grade}, the average grade will be D`;
+
+}
+else if(grade < 80){
+    studentGrade = `The class average is ${grade}, the average grade will be C`;
+
+}
+else if(grade < 90){
+    studentGrade = `T99he class average is ${grade}, the average grade will be B`;
+
+}
+else{
+    studentGrade = `The class average is ${grade}, the average grade will be A`;
+
+}
+85
+console.log(alert(studentGrade));
+
+
 
 
 
