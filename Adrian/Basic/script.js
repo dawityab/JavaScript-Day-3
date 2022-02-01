8dd2657b46cc7bfbe34455fde10b38a2c4fc5fec
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-02-01 07:44:14 +0100

    last files created

--- Adrian/Basic/script.js
+++ Adrian/Basic/script.js
@@ -25,15 +25,27 @@ for (let i = 3; i < value.length; i++) {
 const numberMax = 40
 const tempNew = Math.floor((Math.random() * numberMax) - 5);
 
-console.log(`The temperature is ${tempNew}C.`);
+console.log(`The temperature is ${tempNew}°C`);
 
-if (tempNew > -6, tempNew < 10) {
-    console.log("The wheater is cold");
-} else if (tempNew > 11, tempNew < 26) {
-    console.log("The wheater is moderate");
+if (tempNew >= -5 && tempNew <= 10) {
+    document.getElementById("info").innerHTML = "The weather is cold";
+    
+    let imgWeather = document.getElementById("imgWeather");
+    imgWeather.src = '3.png';
+
+} else if (tempNew >= 10 && tempNew <= 25) {
+    document.getElementById("info").innerHTML = "The weather is moderate";
+
+    let imgWeather = document.getElementById("imgWeather");
+    imgWeather.src = '1.png';
 } else {
-    console.log("The wheather is hot");
+    document.getElementById("info").innerHTML = "The weather is hot" + "imgHot";
+
+    let imgWeather = document.getElementById("imgWeather");
+    imgWeather.src = '2.png';
 }
+
+document.getElementById("degree").innerHTML = (`${tempNew}°C`);
 
 ///
 
